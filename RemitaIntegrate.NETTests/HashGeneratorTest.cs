a18961cbd9f9a3d4ef2cf55ccbf67893913c4ff4
Joseph Izang <joeizang@hotmail.com>
2018-01-22 20:23:08 +0100

    Unit Test for Library

---
+++ RemitaIntegrate.NETTests/HashGeneratorTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemitaIntegrate.NETTests
+{
+    [TestFixture]
+    public class HashGeneratorTest
+    {
+        [Test]
+        public void TestThatHasherReturnsProperHash()
+        {
+
+        }
+    }
+}
---
+++ RemitaIntegrate.NETTests/IntegratorTest.cs
@@ -0,0 +1,28 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using RemitaIntegrate.NET;
+using RemitaIntegrate.NET.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemitaIntegrate.NETTests
+{
+    [TestFixture]
+    public class IntegratorTest
+    {
+
+        [Test]
+        public void TestParameterPreparation()
+        {
+            var rintegrator = Substitute.For<RemitaGateWayIntegrator>();
+
+            rintegrator.Config = Substitute.For<IntegrateConfig>();
+            rintegrator.Config.ApiKey.Returns("abc123");
+            rintegrator.Config.ServiceTypeId.Returns("12345");
+            rintegrator.Config.MerchantId.Returns("6789ab");
+            rintegrator.Config.GateWayUrl.Returns("https://testdomain.local/status");
+
+        }
+    }
+}
